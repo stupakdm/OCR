aa85c1db5820903e50f8c7732809e7bf87c9ef1d
stupak_da <43846458+stupakdm@users.noreply.github.com>
2022-11-17 11:42:05 +0300

    Add files via upload

--- main/modify/Image_update/findBD/data/to_bd.py
+++ main/modify/Image_update/findBD/data/to_bd.py
@@ -2,112 +2,102 @@ from __future__ import annotations
 
 import pandas as pd
 
-from sqlalchemy import create_engine, Integer, String, Column
-from sqlalchemy.orm import Session
-from sqlalchemy import select
-from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-Base = declarative_base()
-
-
-class Namessurnames(Base):
-    __tablename__ = 'namessurnames'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    surname = Column(String(50), nullable=False)
-    priority = Column(Integer, nullable=False)
-
-    def __repr__(self) -> str:
-        return f"Names_Surnames(id={self.id!r}, number={self.name!r}, name={self.surname!r}, priority={self.priority!r}"
-
-class Families(Base):
-    __tablename__ = 'families'
-
-    id = Column(Integer, primary_key=True)
-    family = Column(String(60), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"Families(id={self.id!r}, family={self.family!r}"
-
-class FMSUnit(Base):
-    __tablename__ = 'fmsunit'
-
-    id = Column(Integer, primary_key=True)
-    code = Column(String(10), nullable=False)
-    name = Column(String(200), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"FMSUnit(id={self.id!r}, code={self.code!r}, name={self.name!r}"
-
-class Cities(Base):
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    regionname = Column(String(75), nullable=False)
-
-    def __repr__(self) -> str:
-        return f"Cities(id={self.id!r}, name={self.name!r}, region_name={self.regionname!r}"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Namessurnames, Families, FMSUnit, Cities, Base
 
 
+# Создать Базу данных
+def create_db():
+    connection = psycopg2.connect(user="postgres", password="postgres")
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
+    cursor = connection.cursor()
+    cursor.execute('create database sqlalchemypassport')
+    cursor.close()
+    connection.close()
+
+# Создать все таблицы в БД
 def create_table():
-    engine = create_engine("postgresql+psycopg2://postgres:1111@localhost/sqlalchemy_passport")
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/sqlalchemypassport")
     Base.metadata.create_all(engine)
 
+# Удалить все таблицы в БД
+def drop_all_table():
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/sqlalchemypassport")
+    Base.metadata.drop_all(engine)
+
+# Добавить данные в БД
 def append_data():
-    engine = create_engine("postgresql+psycopg2://postgres:1111@localhost/sqlalchemy_passport")
-
-    with Session(engine) as session:
-        all_list = []
-
-
-        df_name = pd.read_csv(f'Names_and_surnames2.csv', encoding='utf-8', sep=',')
-        for i, row in df_name.iterrows():
-            item = Namessurnames(
-                name=row['name'],
-                surname=row['surname'],
-                priority=row['priority'],
-            )
-            all_list.append(item.copy())
-
-        df_families = pd.read_csv(f'families3.csv', encoding='utf-8')
-        for i, row in df_families.iterrows():
-            item = Namessurnames(
-                family = row['family'],
-            )
-            all_list.append(item.copy())
-
-        df_fms = pd.read_csv(f'fms_unit.csv', sep=',', index_col=False, encoding='utf-8')
-        for i, row in df_fms.iterrows():
-            item = FMSUnit(
-                code=row['code'],
-                name = row['name'],
-            )
-            all_list.append(item.copy())
-
-        df_cities = pd.read_csv(f'RussianCities.tsv', sep='\t', index_col=False, encoding='utf-8')
-        for i, row in df_cities.iterrows():
-            item = Cities(
-                name=row['name'],
-                regionname = row['region_name'],
-            )
-            all_list.append(item.copy())
-
-        session.add_all(all_list)
-
-        session.commit()
-
-def get_data(model = None):
-    engine = create_engine("postgresql+psycopg2://postgres:1111@localhost/sqlalchemy_passport")
-
-    if model != None:
-        session = Session(engine)
-        result = session.execute(select(model))
-        session.close()
-        return result
-    else:
-        return None
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/sqlalchemypassport")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    #with Session(engine) as session:
+    all_list = []
 
 
+    df_name = pd.read_csv(f'Names_and_surnames2.csv', encoding='utf-8', sep=',')
+    for i, row in df_name.iterrows():
+        item = Namessurnames(
+            name=row['name'],
+            surname=row['surname'],
+            priority=row['priority'],
+        )
+        all_list.append(item)
+
+    df_families = pd.read_csv(f'families3.csv', encoding='utf-8')
+    for i, row in df_families.iterrows():
+        item = Families(
+            family = row['family'],
+        )
+        all_list.append(item)
+
+    df_fms = pd.read_csv(f'fms_unit.csv', sep=',', index_col=False, encoding='utf-8')
+    for i, row in df_fms.iterrows():
+        item = FMSUnit(
+            code=row['code'],
+            name = row['name'],
+        )
+        all_list.append(item)
+
+    df_cities = pd.read_csv(f'RussianCities.tsv', sep='\t', index_col=False, encoding='utf-8')
+    for i, row in df_cities.iterrows():
+        item = Cities(
+            name=row['name'],
+            regionname = row['region_name'],
+        )
+        all_list.append(item)
+
+    session.add_all(all_list)
+
+    session.commit()
+    session.close()
+
+# Достать все данные из БД
+def get_items():
+    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost/sqlalchemypassport")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    models = [Namessurnames, Families, FMSUnit, Cities]
+    for model in models:
+        print(model)
+        for item in session.query(model).all():
+            for values in model.__dict__.keys():
+                if values[0] != '_' and values != 'id':
+                    print(item.__dict__[values], end=' ')
+            print()
+        print()
+
+    session.close()
+
+
+
+if __name__ == '__main__':
+    create_db()
+    drop_all_table()
+    create_table()
+    append_data()
+    get_items()
